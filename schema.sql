5c881606e96d69391b4aeafcca6d9bb5f987b595
Eduardo Villarreal <evillaz123@gmail.com>
2023-05-16 18:23:47 -0500

    Create animals table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+  id INTEGER,
+  name VARCHAR(255),
+  date_of_birth DATE,
+  escape_attempts INTEGER,
+  neutered BIT,
+  weight_kg DECIMAL(10, 2)
 );
