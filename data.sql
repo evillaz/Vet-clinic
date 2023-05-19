98f05621d28f94a986ff5dbc9d7335eccc3f8543
evillaz <evillaz123@gmail.com>
2023-05-19 03:42:29 -0500

    Insert data owners species update animals

--- data.sql
+++ data.sql
@@ -1,16 +1,45 @@
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES
-   (1, 'Agumon', '2020-02-03', 0, true, 10.23),
-   (2, 'Gabumon', '2018-11-15', 2, true, 8),
-   (3, 'Pikachu', '2021-1-7', 1, false, 15.04),
-   (4, 'Devimon', '2017-5-12', 5, true, 11);
+  ('Agumon', '2020-02-03', 0, true, 10.23),
+  ('Gabumon', '2018-11-15', 2, true, 8),
+  ('Pikachu', '2021-1-7', 1, false, 15.04),
+  ('Devimon', '2017-5-12', 5, true, 11);
 
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) 
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES
+  ('Charmander', '2020-02-08', 0, false, -11),
+  ('Plantmon', '2021-11-15', 2, true, -5.7),
+  ('Squirtle', '1993-04-02', 3, false, -12.13),
+  ('Angemon', '2005-06-12', 1, true, -45),
+  ('Boarmon', '2005-06-07', 7, true, 20.4),
+  ('Blossom', '1998-10-13', 3, true, 17),
+  ('Ditto', '2022-05-14', 4, true, 22);
+
+INSERT INTO owners (full_name, age)
 VALUES 
-(5, 'Charmander', '2020-02-08', 0, false, -11),
-(6, 'Plantmon', '2021-11-15', 2, true, -5.7),
-(7, 'Squirtle', '1993-04-02', 3, false, -12.13),
-(8, 'Angemon', '2005-06-12', 1, true, -45),
-(9, 'Boarmon', '2005-06-07', 7, true, 20.4),
-(10, 'Blossom', '1998-10-13', 3, true, 17),
-(11, 'Ditto', '2022-05-14', 4, true, 22);
+  ('Sam Smith', 34),
+  ('Jennifer Orwell', 19),
+  ('Bob', 45),
+  ('Melody Pond', 77),
+  ('Dean Winchester', 14),
+  ('Jodie Whittaker', 38);
+	
+INSERT INTO species (name)
+VALUES 	
+  ('pokemon'),
+	('digimon');
+
+UPDATE animals
+SET species_id = CASE 
+    WHEN name LIKE '%mon' THEN (SELECT id FROM species WHERE name = 'digimon')
+    ELSE (SELECT id FROM species WHERE name = 'pokemon')
+  END;
+
+UPDATE animals
+SET owner_id = CASE 
+    WHEN name = 'Agumon' THEN (SELECT id FROM owners WHERE full_name = 'Sam Smith')
+    WHEN name IN ('Gabumon', 'Pikachu') THEN (SELECT id FROM owners WHERE full_name = 'Jennifer Orwell')
+    WHEN name IN ('Devimon', 'Plantmon') THEN (SELECT id FROM owners WHERE full_name = 'Bob')
+    WHEN name IN ('Charmander', 'Squirtle', 'Blossom') THEN (SELECT id FROM owners WHERE full_name = 'Melody Pond')
+    WHEN name IN ('Angemon', 'Boarmon') THEN (SELECT id FROM owners WHERE full_name = 'Dean Winchester')
+  END;
